802e61ca7b46806d29eb662d4a2b612e31c072f8
Clement <cpoh@unimelb.edu.au>
2012-05-20 16:02:48 +1000

    Lots of stuff

--- extract.py
+++ extract.py
@@ -3,6 +3,7 @@ import sqlite3
 
 from csv    import reader
 from re     import compile
+from math   import log10
 
 PATH = './train/'
 
@@ -11,18 +12,20 @@ conn = sqlite3.connect('./knowledge.db')
 conn.text_factory = str
 c = conn.cursor()
 
-delims_r    = compile(r"[\W_\d]+")
-title_r     = compile("Title: (.*)")
-author_r    = compile("Author: (.*)")
+r_delims    = compile(r"[\W_\d]+")
+r_title     = compile("Title: (.*)")
+r_author    = compile("Author: (.*)")
 
 def create_tables():
     c.execute('''DROP TABLE IF EXISTS words''')
     c.execute('''
-        CREATE TABLE IF NOT EXISTS words 
+        CREATE TABLE IF NOT EXISTS words
             ( word      TEXT PRIMARY KEY
             , freq      INTEGER
+            , books     INTEGER
+            , idf       REAL
             , UNIQUE    (word)
-        )''') 
+        )''')
 
     c.execute('''DROP TABLE IF EXISTS books''')
     c.execute('''
@@ -32,7 +35,7 @@ def create_tables():
             , author    TEXT
             , category  TEXT
             , total     INTEGER
-            , dist      INTEGER
+            , diff      INTEGER
             , UNIQUE    (file)
         )''')
 
@@ -42,58 +45,98 @@ def create_tables():
             ( file      TEXT
             , word      TEXT
             , freq      INTEGER
-            , FOREIGN KEY (file) REFERENCES books(file)
-            , FOREIGN KEY (word) REFERENCES words(word)
+            , tf        REAL
+            , FOREIGN KEY (file) REFERENCES books(file) ON DELETE CASCADE
         )''') 
 
 def unique_tokens(text):
     total       = 0
-    distinct    = 0
+    diff        = 0
     words       = {}
-    tokens      = delims_r.split(text)
+    tokens      = r_delims.split(text)
     for t in tokens:
-        if len(t) > 4:
+        if len(t) > 3:
             total += 1
             try:
                 words[t] += 1
             except:
-                words[t] = 1
-                distinct += 1
-    return total, distinct, words 
+                diff     += 1
+                words[t] = 1.0
+
+    return total, diff, words 
+
 
 def preprocess_book(file, cat): 
     path = os.path.join(PATH, file)
     text = open(path).read()
 
-    title = title_r.search(text)
+    title = r_title.search(text)
     title = title.group(1)[:-1] if title else None
 
-    author = author_r.search(text)
-    author = author.group(1)[:-1] if author else None
+    auth = r_author.search(text)
+    auth = auth.group(1)[:-1] if auth else None
 
-    total, distinct, tokens = unique_tokens(text.lower())
+    total, diff, tokens = unique_tokens(text.lower())
 
-    c.execute('''INSERT INTO books (file, title, author, category, total, dist)
-        VALUES (?, ?, ?, ?, ?, ?)''' 
-        , [file, title, author, cat, total, distinct])
+    c.execute('''INSERT INTO books (file, title, author, category, total, diff)
+        VALUES (?, ?, ?, ?, ?, ?)''', [file, title, auth, cat, total, diff])
 
-    return tokens
+    print file, title, auth, cat, total
+    return tokens, total
 
 
 def process_books():
+    nbooks = 0.0
     for (file, cat) in reader(open("train.class")):
-        tokens = preprocess_book(file, cat)
+        tokens, total = preprocess_book(file, cat)
 
         for (tok, freq) in tokens.items(): 
-            c.execute('''INSERT OR REPLACE into words (word, freq) VALUES (?, 
-                COALESCE (
-                    (SELECT freq FROM words WHERE word = ?), 0) + ?)''',
-                [tok, tok, freq]) 
-            c.execute('''INSERT INTO book_words (file, word, freq) VALUES
-                (?, ?, ?)''', [file, tok, freq])
+            c.execute('''INSERT INTO book_words (file, word, freq, tf) VALUES
+                (?, ?, ?, ?)''', [file, tok, freq])
 
+        nbooks += 1
+    return nbooks
+
+
+def populate_words(nbooks):
+    # Get all the words which appear in more than one book.
+    c.execute('''SELECT word, sum(freq), count(*) AS books
+            FROM book_words
+            GROUP BY word
+            HAVING books > 1
+        ''')
+
+    for (word, freq, books) in c.fetchall():
+        c.execute('''INSERT INTO words (word, freq, books, idf)
+            VALUES (?, ?, ?, ?)''', [word, freq, books, log10(nbooks/books)])
+    
+
+def cat_analysis():
+    c.execute('''DROP TABLE IF EXISTS categories''')
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS categories AS
+            SELECT category as category
+                , sum(total) as total
+                FROM books, book_words
+                GROUP BY category, word''')
+
+    c.execute('''DROP TABLE IF EXISTS cat_words''')
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS cat_words AS
+            SELECT categories.category AS category
+                , word AS word
+                , cast(sum(freq) as real)/categories.total AS tf
+            FROM book_words, books, categories
+            WHERE book_words.file = books.file 
+                AND categories.category = books.category
+            GROUP BY categories.category, word
+        ''')
 
 create_tables()
-process_books()
+nbooks = process_books()
+print "Calculating idfs for unique each term."
+populate_words(nbooks)
+print "Calculating category tfs"
+cat_analysis()
 
 conn.commit()
